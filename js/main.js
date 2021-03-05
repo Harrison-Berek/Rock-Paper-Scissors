5240e2072ae80146be83056b8ccbfdf309fe94a3
Harrison-Berek <hcberek@gmail.com>
2021-03-05 10:24:47 -0700

    Create Constants

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 /*----- constants -----*/
-
+const rpsLookup = {
+    r: {
+        imgUrl: 'imgs/rock.png',
+        beats: 's'
+    },
+    p: {
+        imgUrl: 'imgs/paper.png',
+        beats: 'r'
+    },
+    s: {
+        imgUrl: 'imgs/scissors.png"'
+        beats: 'p'
+    }
+};
 
 /*----- app's state (variables) -----*/
 
